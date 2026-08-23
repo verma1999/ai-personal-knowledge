3f0fef2902c630d7a7c9aaba7ca1f685a8c49bb1
Aditya Verma <verma.aditya1999@gmail.com>
2026-08-23 22:45:25 +0530

    chunking logic

--- document-processor/main.py
+++ document-processor/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from models.document import Document
 from uuid import uuid4
+from models.chunk import Chunk
 
 
 DOCS_DIR = Path("../docs")
@@ -22,7 +23,55 @@ def read_document(file) -> Document:
             content=content
         )
 
+def chunk_document(document) -> list[Chunk]:
+        index = 0
+        chunk_size = 100
+        overlap = 1 #overlap sentences
+        chunks = []
+        current_chunk = []
+        sentences = [
+            sentence.strip() + "."
+            for sentence in document.content.split(".")
+            if sentence.strip()
+        ]
+        for sentence in sentences:
+            current_chunk_length = len(" ".join(current_chunk))
+            if current_chunk_length + len(sentence) <= chunk_size:
+                current_chunk.append(sentence)
+            else:
+                if current_chunk:
+                    chunks.append(
+                        Chunk(
+                            id=str(uuid4()),
+                            document_id=document.id,
+                            content=" ".join(current_chunk),
+                            chunk_index=index,
+                            metadata={}
+                        )
+                    )
+                    index += 1
+                overlap_text = current_chunk[-overlap:]
+                current_chunk = overlap_text.copy()
+                current_chunk.append(sentence)
+
+        if current_chunk:
+            chunks.append(
+                Chunk(
+                    id=str(uuid4()),
+                    document_id=document.id,
+                    content=" ".join(current_chunk),
+                    chunk_index=index,
+                    metadata={}
+                )
+            )
+        return chunks
+
 
 if __name__ == "__main__":
     find_documents()
-    print(documents)
+    #print(documents)
+    for document in documents:
+        chunks = chunk_document(document)
+        print(f"\nDOCUMENT: {document.path}")
+        for chunk in chunks:
+            print(chunk)
